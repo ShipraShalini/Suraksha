2e4ab61ac85288b2ea8637146202f4504704c2bb
Shipra <code.shipra@gmail.com>
2017-04-14 15:30:36 +0530

    game template changes

--- game/views.py
+++ game/views.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.views.generic import FormView
+
+from game.forms import UserForm
+
+
+class User(FormView):
+    template_name = 'notification_form.html'
+    form_class = UserForm
+
+    def form_valid(self, form):
+        job_id = form.schedule_notification()
+        return render_to_response("success.html", context={"job_id_scheduled": job_id})
+    pass
+
 
 
 
